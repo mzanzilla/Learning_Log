0f74d6c4194a6414454020c5d9c1f657357d0eda
Mike <m.mensah@hotmail.com>
2018-10-23 18:34:15 -0400

    Entry model added

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
     def __str__(self):
     #return a string representation of the model
         return self.text
+
+#Entry class provides something specific about a topic
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Entries'
+
+    def __str__(self):
+    #return a string representation of the Model
+        return self.text[:50] + "..."
