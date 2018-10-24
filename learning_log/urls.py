a152256485a9ecf75dfae4fd17c74f79aa0b2ff9
Mike <m.mensah@hotmail.com>
2018-10-24 16:09:14 -0400

    base html added as the parent html from which child html will inherit froom. Topic html added. All links are working

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
 
+
+app_name = "learning_logs"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path(r'', learning_log.urls),
     path(r'', include('learning_logs.urls')),
 ]
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from .import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^topics/$', views.topics, name = 'topics'),
 ]
