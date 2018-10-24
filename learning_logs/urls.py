53c008e16cf609f64c8e3bf5c58a7c2a692d97f1
Mike <m.mensah@hotmail.com>
2018-10-24 19:50:44 -0400

    topic html added to provide more details on selected topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,4 +4,7 @@ from .import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^topics/$', views.topics, name = 'topics'),
+    # Detail page for a single topic
+    url(r'^topic/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
 ]
