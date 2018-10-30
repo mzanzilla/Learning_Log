92ea7ac5c6ce169c6b320768c57a0c8adc037002
Mike <m.mensah@hotmail.com>
2018-10-29 21:38:50 -0400

    edit_entry.html added to allow users to edit their entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Topic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm, Entry
 
 def index(request):
     """The home page for Learning Log."""
@@ -34,3 +34,37 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic',args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    #edit an existing entry
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #Initial request; prefill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        #Post data submitted, process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic',args=[topic.id] ))
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html', context)
