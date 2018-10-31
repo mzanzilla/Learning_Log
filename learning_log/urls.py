9b63aebab2f48358608ab835c95c7ddb099b1bf4
Mike <m.mensah@hotmail.com>
2018-10-31 12:49:34 -0400

    user registration form added

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 app_name = "learning_logs"
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path(r'', learning_log.urls),
+    path(r'', include('users.urls')),
+    url(r'^users/', include('users.urls')),
     path(r'', include('learning_logs.urls')),
 ]
