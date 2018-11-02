912833a0ac2f71d136b8c34f272187dc4637fcda
Mike <m.mensah@hotmail.com>
2018-11-02 13:42:28 -0400

    Access to posting have been restricted to individual users. Users can only see what they posted.

--- users/views.py
+++ users/views.py
@@ -28,35 +28,3 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'users/register.html', context)
-
-
-
-# from django.http import HttpResponseRedirect
-# from django.urls import reverse
-# from django.contrib.auth import login, logout, authenticate
-# from django.shortcuts import render
-# from django.contrib.auth.forms import UserChangeForm
-#
-#
-# def logout_view(request):
-#     #Log the user out
-#     logout(request)
-#     return HttpResponseRedirect(reverse('index'))
-#
-# def register(request):
-#     #register a new users
-#     if request.method != 'POST':
-#         #display blank register forms
-#         form = UserCreationForm()
-#     else:
-#         #process completed form
-#         form = UserCreationForm(data = request.POST)
-#
-#         if form.is_valid():
-#             new_user = form.save()
-#             #Log the user in and redirect to homepage
-#             authenticated_user = authenticate(username = new_user.username, password=request.POST['password1'])
-#             login(request, authenticated_user)
-#             return HttpResponseRedirect(reverse('index'))
-#         context = {'form': form}
-#         return render(request, 'users/register.html', context)
